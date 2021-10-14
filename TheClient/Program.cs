674dab17de7cbbb6f429721f9ae721139b880f15
Lucas Teles <lucas.teles@outlook.com>
2021-10-14 20:04:02 -0300

    WIP

---
+++ TheClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Console;
+
+WriteLine("Press enter to connect");
+ReadLine();
+
+var port = 9000;
+var endpoint = new IPEndPoint(IPAddress.Loopback, port);
+
+var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+socket.Connect(endpoint);
+
+await using NetworkStream networkStream = new (socket, true);
+
+var msg = "Hello world";
+
+var buffer = Encoding.UTF8.GetBytes(msg);
+await networkStream.WriteAsync(buffer);
+
+var response = new byte[1024];
+
+await networkStream.ReadAsync(response);
+
+var responseStr = Encoding.UTF8.GetString(response);
+
+WriteLine($"Response: {responseStr}");
+
+ReadLine();
---
+++ TheServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+void Start(int port = 9000)
+{
+    var endPoint = new IPEndPoint(IPAddress.Loopback, port);
+    var socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    socket.Bind(endPoint);
+    socket.Listen(128);
+
+    _ = Task.Run(() => DoEcho(socket));
+}
+
+async Task DoEcho(Socket socket)
+{
+    while (true)
+    {
+        var clientSocket = await Task.Factory.FromAsync(
+            socket.BeginAccept,
+            socket.EndAccept,
+            null
+        ).ConfigureAwait(false);
+        WriteLine("ECHO SERVER :: CLIENT CONNECTED");
+
+        await using var stream = new NetworkStream(clientSocket, true);
+        var buffer = new byte[1024];
+
+        while (true)
+        {
+            var bytesRead = await stream.ReadAsync(buffer).ConfigureAwait(false);
+            if (bytesRead == 0) break;
+
+            var received = Encoding.UTF8.GetString(buffer);
+            WriteLine($"Received: {received}");
+            await stream.WriteAsync(buffer).ConfigureAwait(false);
+        }
+    }
+}
+
+
+Start();
+WriteLine("Echo Server running...");
+ReadLine();
